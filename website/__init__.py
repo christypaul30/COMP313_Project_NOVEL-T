d00e9df790e105f2462fa1e3edad8e4dccdd0a6a
schen196 <schen196@my.centennialcollege.ca>
2022-11-10 17:16:46 -0500

    import bookmarkedchapters to models

--- website/__init__.py
+++ website/__init__.py
@@ -30,7 +30,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, CharacterSheet, Book, BookChapters, BookGenres, Library
+    from .models import User, CharacterSheet, Book, BookChapters, BookGenres, Library, BookmarkedChapters
 
     # create_database(app)
     with app.app_context():
